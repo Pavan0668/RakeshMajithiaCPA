4e5c1b7cf735d52a8510d32c19dc361765f653bd
Pavan853008 <pavansaudagar27@gmail.com>
2024-12-07 17:28:19 +0530

    first commit

---
+++ Content/Website-Content/js/Chatscript.js
@@ -0,0 +1,19 @@
+﻿// Get the chatbot icon and chatbox elements
+const chatIcon = document.getElementById('chatIcon');
+const chatbox = document.getElementById('chatbox');
+const floatbox = document.getElementById('floatingMessage');
+// Toggle the visibility of the chatbox when the icon is clicked
+chatIcon.addEventListener('click', () => {
+    chatbox.classList.toggle('show');
+    
+});
+
+// Close the chatbox if clicked outside
+window.addEventListener('click', function (event) {
+    if (!chatbox.contains(event.target) && !chatIcon.contains(event.target)) {
+        chatbox.classList.remove('show');
+        floatbox.classList.remove('show');
+
+    }
+});
+
